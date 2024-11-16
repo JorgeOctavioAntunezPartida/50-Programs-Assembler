cf8f2e18f54446dba47acbb8b20d51d3e45a1fb7
JorgeOctavioAntunezPartida <159859506+JorgeOctavioAntunezPartida@users.noreply.github.com>
2024-11-16 12:07:52 -0800

    Create Program-32.s

---
+++ Program-32.s
@@ -0,0 +1,12 @@
+// Programa: 
+// Alumno: Antunez Partida Jorge Octavio - 21211910
+// Carrera: Ing. Sistemas computacionales
+
+// asciinema: 
+
+// Programa en C#
+/*
+
+*/
+
+// Programa en Ensamblador
